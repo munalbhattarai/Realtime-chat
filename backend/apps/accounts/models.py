1114d23d8d85af2a3cce944d391a1ec576d4f59f
Munal Bhattarai <emailaddress9810@gmail.com>
2026-08-20 07:00:16 +0545

    added the user profile model

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    profile_picture = models.ImageField( upload_to= "profile_picture/", blank=True , null=True)
+    
+    bio = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.username
